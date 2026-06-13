8cf46ac96c284df57ab325503af063137a264edf
Kyryloloshka <kirill.contrmptuous@gmail.com>
2026-06-13 22:39:07 +0300

    refactor: introduce Hash type

---
+++ src/core/hash.rs
@@ -0,0 +1,41 @@
+use serde::{Deserialize, Serialize};
+use std::fmt;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize, Hash)]
+pub struct Hash(pub [u8; 20]);
+
+impl fmt::Display for Hash {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", hex::encode(self.0))
+    }
+}
+
+impl From<[u8; 20]> for Hash {
+    fn from(bytes: [u8; 20]) -> Self {
+        Self(bytes)
+    }
+}
+
+impl AsRef<[u8; 20]> for Hash {
+    fn as_ref(&self) -> &[u8; 20] {
+        &self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_hash_display() {
+        let h = Hash([0xde, 0xad, 0xbe, 0xef, 0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff]);
+        assert_eq!(format!("{}", h), "deadbeef00112233445566778899aabbccddeeff");
+    }
+
+    #[test]
+    fn test_hash_from_bytes() {
+        let bytes = [1u8; 20];
+        let h = Hash::from(bytes);
+        assert_eq!(h.0, bytes);
+    }
+}
--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,7 +1,9 @@
+pub mod hash;
 pub mod models;
 pub mod objects;
 pub mod storage;
 
+pub use hash::Hash;
 pub use models::*;
 pub use objects::*;
 pub use storage::*;
