0895664f0e72ea2f17c5f20a213106ef730fc7b9
Kyryloloshka <kirill.contrmptuous@gmail.com>
2026-06-14 10:25:55 +0300

    feat: add GIK_VERSION constant for version reporting

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,7 @@
 pub const DB_PATH: &str = ".gik.db";
 pub const DEFAULT_AUTHOR_NAME: &str = "Gik User";
 pub const DEFAULT_AUTHOR_EMAIL: &str = "user@gik.local";
+
+/// The current version of Gik, sourced from Cargo.toml at compile time.
+pub const GIK_VERSION: &str = env!("CARGO_PKG_VERSION");
+
